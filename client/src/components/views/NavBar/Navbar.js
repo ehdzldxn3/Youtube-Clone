74f70e43bfc0cb4f158e0b7167053e35344c11a0
ehdzldxn3 <77223991+ehdzldxn3@users.noreply.github.com>
2021-11-21 16:21:43 +0900

    211121

--- client/src/components/views/NavBar/Navbar.js
+++ client/src/components/views/NavBar/Navbar.js
@@ -1,31 +1,53 @@
-import React, { } from 'react'
-import { Box, AppBar, Toolbar, Typography, IconButton, Button, } from '@material-ui/core';
-import { MenuIcon } from '@material-ui/icons/Menu';
+import React, { useState } from 'react';
+import LeftMenu from './Sections/LeftMenu';
+import RightMenu from './Sections/RightMenu';
+import { Drawer, Button, Icon } from 'antd';
+import './Sections/Navbar.css';
 
+function NavBar() {
+  const [visible, setVisible] = useState(false)
 
+  const showDrawer = () => {
+    setVisible(true)
+  };
 
-function Navbar() {
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              News
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
-        )
+  const onClose = () => {
+    setVisible(false)
+  };
+
+  return (
+    <nav className="menu" style={{ position: 'fixed', zIndex: 5, width: '100%' }}>
+      <div className="menu__logo">
+        <a href="/">Logo</a>
+      </div>
+      <div className="menu__container">
+        <div className="menu_left">
+          <LeftMenu mode="horizontal" />
+        </div>
+        <div className="menu_rigth">
+          <RightMenu mode="horizontal" />
+        </div>
+        <Button
+          className="menu__mobile-button"
+          type="primary"
+          onClick={showDrawer}
+        >
+          <Icon type="align-right" />
+        </Button>
+        <Drawer
+          title="Basic Drawer"
+          placement="right"
+          className="menu_drawer"
+          closable={false}
+          onClose={onClose}
+          visible={visible}
+        >
+          <LeftMenu mode="inline" />
+          <RightMenu mode="inline" />
+        </Drawer>
+      </div>
+    </nav>
+  )
 }
 
-export default Navbar
+export default NavBar
